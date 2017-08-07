9c3dacbb86245361ad8eeb5e0740a2b65e5f6300
武田 憲太郎 <takeda@youmind.jp>
2017-08-07 14:12:51 +0900

    クライアント側実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<code *ngFor="let log of logs">{{ log | json }}</code>`,
+  styles: ['code { display: block; }']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor( private http :HttpClient ) {}
+  public readonly logs: any[] = [];
+
+  ngOnInit() {
+    const request = new HttpRequest('GET', 'http://localhost:8000', {
+      reportProgress: true,
+      responseType: 'text'
+    });
+
+    this.http.request(request).subscribe( this.logs.push.bind(this.logs) );
+  }
 }
